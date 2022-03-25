60942b973fe24d6ebfb0130d6c8c2a5b39fe2f2e
Otybrian <brian.otieno@student.moringaschool.com>
2022-03-26 01:17:21 +0300

    add the methods and class methods for the image model class

--- photos/models.py
+++ photos/models.py
@@ -14,3 +14,35 @@ class Image(models.Model):
     link = models.CharField(max_length=250)
     location = models.ForeignKey(Category,on_delete=models.CASCADE)
     category =models.ForeignKey(Location,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_image(cls):
+        image = Image.objects.get_or_create()
+        return image
+
+    @classmethod
+    def get_image(cls, image_id):
+        image = cls.objects.filter(id = image_id)
+        return image
+
+    @classmethod
+    def search_by_category(cls, search_term):
+        image = cls.objects.filter(category__icontains = search_term)
+        return image
+
+    @classmethod
+    def filter_by_location(cls, location):
+        image = cls.objects.filter(location = location)
+        return image
+
+
+
