64e911ae2beb435d956dfb3c3c5d005a7214802f
Otybrian <brian.otieno@student.moringaschool.com>
2022-03-26 01:19:51 +0300

    add the save and delete methods for the model class category

--- photos/models.py
+++ photos/models.py
@@ -4,6 +4,15 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
 class Location(models.Model):
     name = models.CharField(max_length=50)
 
