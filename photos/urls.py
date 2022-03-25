88b691bc84082bcf685e27a61ebd491dae01db40
Otybrian <brian.otieno@student.moringaschool.com>
2022-03-26 01:52:17 +0300

    create the gallery method that displays photos by category in the views.py

--- photos/urls.py
+++ photos/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 ulrpatterns = [
     url(r'^$', views.gallery, name = 'gallery'),
+    url(r'^viewPhoto$/', views.viewPhoto, name = 'viewPhoto'),
     url(r'^search/', views.search_results, name = 'search_results')
 
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
 import datetime as dt
+from .models import Category, Location, Image
 
 
 
 # Create your views here.
 
 def gallery(request):
+    category = request.GET.get('category')
+    if category==None:
+        photos=Image.objects.all()
+    else:
+        photos=Image.objects.filter(category__name = category)
+        images = Category.objects.all()
+        display={'images':images, 'photos': photos}
 
-
-    return render(request, 'gallery.html')
+    return render(request, 'gallery.html', display)
 
 def search_results(request):
 
 
     return render(request, 'search.html')
+
+
