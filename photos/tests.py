7f543bb1a116d6c3c7303c650fc92ff8820bb05b
Otybrian <brian.otieno@student.moringaschool.com>
2022-03-26 05:56:55 +0300

    make database migrations

--- photos/tests.py
+++ photos/tests.py
@@ -1,12 +1,28 @@
 from django.test import TestCase
-from photos.models import Image,Location,Category
+from photos.models import Image,Location,category
 
 
 # Create your tests here.
+class LocationTestClass(TestCase):
 
-class ImageTestClass(TestCase):
-    #setup method
+    # Set up method
     def setUp(self):
-        self.myImage=Image(image = 'image', name = 'Nature', description='Nature is beautiful')
+        self.nairobi= Location(name = 'Nairobi')
 
-    #Test instance
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.nairobi,Location))
+
+     # Testing Save Method for locations
+    def test_save_location(self):
+        self.nairobi.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+
+# class ImageTestClass(TestCase):
+#     #setup method
+#     def setUp(self):
+#         self.myImage=Image(image = 'image', name = 'Nature', description='Nature is beautiful')
+
+#     #Test instance
